4b74cf89cb930e3cab6acc16c10542e71df85f7e
Lucas Cervelli Haderne <lucasch98.lch@gmail.com>
2024-01-08 20:31:12 -0300

    ModalFilmData

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,16 @@ import TitlePage from './components/titlePage'
 import NavbarPage from './components/navbarPage'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Card from 'react-bootstrap/Card';
-import { FaCircle } from "react-icons/fa6";
 import { PiMedalFill } from "react-icons/pi";
-import Badge from 'react-bootstrap/Badge';
-import Stack from 'react-bootstrap/Stack';
 import { FaCalendarAlt } from "react-icons/fa";
+import { FaFilm } from "react-icons/fa6";
+import { PiFilmSlateBold } from "react-icons/pi";
 
 
 //API
 const API_KEY = '36f0395abc3500cf095451af07ed4080'
 const API_CALL_SEARCH = 'https://api.themoviedb.org/3/search/movie?query='
+const GENRE_URL = 'https://api.themoviedb.org/3/movie/'
 const API_IMG = 'https://image.tmdb.org/t/p/w300/'
 //https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=36f0395abc3500cf095451af07ed4080
 function App() {
@@ -25,6 +24,7 @@ function App() {
   const handleShow = () => setShowModal(true);
   
   const [dataFilm, setDataFilm] = useState(null)
+  const [genre, setGenre] = useState(null)
 
   const searchFilm = async (film) => {
       try {
@@ -36,7 +36,12 @@ function App() {
         if(dataFilm['results'].length === 0){
           throw new Error('Error en la solicitud a la API')
         }
+
+        const responseGenre = await fetch(`${GENRE_URL}${dataFilm['results'][0]['id']}${'?api_key='}${API_KEY}`)
+        const dataGenre = await responseGenre.json()
+        setGenre(dataGenre['genres'][0]['name'])
         modalDataFilm(dataFilm)
+
       } catch (error) {
         setDataFilm(null);
         alert("No data for this film");
@@ -60,17 +65,21 @@ function App() {
               <Modal.Header style={{backgroundColor: "black", color: "white"}}>
                 <h3 className='titleModal'>{dataFilm['results'][0]['original_title']}</h3>
               </Modal.Header>
-              <Modal.Body style={{backgroundColor: "black", color: "white"}}>      
-                <ul>
-                  <div style={{display: "flex"}}>
-                    <img style={{width: "200px"}} src={`${API_IMG}${dataFilm['results'][0]['poster_path']}`}/>
-                      <h5><PiMedalFill style={{ color: "#a78BFa"}}/>{Math.floor(dataFilm['results'][0]['vote_average'])}</h5>
-                      <h5><FaCalendarAlt style={{color: "#a78BFa", marginLeft: "10px"}}/>{dataFilm['results'][0]['release_date']}</h5>
+              <Modal.Body style={{backgroundColor: "black", color: "white"}}>  
+                <div class="container mt-4">
+                  <div class="row">
+                    <div class="col-md-4 mb-4">
+                      <img src={`${API_IMG}${dataFilm['results'][0]['poster_path']}`} class="img-fluid" alt="Foto"/>
+                    </div>
+                    <div className="col-md-8">
+                      <h4><PiMedalFill style={{ color: "#a78BFa"}}/> {Math.floor(dataFilm['results'][0]['vote_average'])} Points</h4>
+                      <h4><FaCalendarAlt style={{color: "#a78BFa"}}/> {dataFilm['results'][0]['release_date']}</h4>
+                      <h4><FaFilm style={{color: "#a78BFa"}}/> {genre}</h4>
+                      <h4><PiFilmSlateBold style={{color: "#a78BFa"}}/> {dataFilm['results'][0]['overview']}</h4>
+                      <p></p>
+                    </div>
                   </div>
-                  <li>
-                    <h5 style={{color: "white", marginTop: "30px", marginLeft: "25px", fontFamily: "sans-serif", fontWeight: "bold"}}>{dataFilm['results'][0]['overview']}</h5>
-                  </li>
-                </ul>
+                </div>
               </Modal.Body>
               <Modal.Footer style={{backgroundColor: "black", color: "white"}}>
                 <Button variant='light' onClick={handleClose}>
